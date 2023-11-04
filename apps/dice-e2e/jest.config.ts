a87beab3c09b24e6b084b7b6e305a4771e15e0af
Sergei <dev.sergeialimov@gmail.com>
2023-11-04 09:39:52 +0400

    Initial commit

---
+++ apps/dice-e2e/jest.config.ts
@@ -0,0 +1,19 @@
+/* eslint-disable */
+export default {
+  displayName: 'dice-e2e',
+  preset: '../../jest.preset.js',
+  globalSetup: '<rootDir>/src/support/global-setup.ts',
+  globalTeardown: '<rootDir>/src/support/global-teardown.ts',
+  setupFiles: ['<rootDir>/src/support/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/dice-e2e',
+};
